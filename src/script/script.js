b9629df519e22d6b184d3685bf89247231b55ce5
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-02-29 15:39:55 -0300

    feat: added function color and font

--- src/script/script.js
+++ src/script/script.js
@@ -1,6 +1,8 @@
 var pomodoro = 0;
 var short = 0;
 var long = 0;
+var font = "";
+var color = "";
 
 function Apply(){
     pomodoro = document.getElementById('pomodoro').value;
@@ -9,6 +11,29 @@ function Apply(){
     console.log(pomodoro);
     console.log(short);
     console.log(long);
+    document.getElementById("principal").style.backgroundColor = color;
 }
 
-''
+function font1(){
+    //font = fonte tal
+}
+
+function font2(){
+    //font = fonte tal
+}
+color
+function font3(){
+    //font = fonte tal
+}
+
+function color1(){
+    color = "#f8706f";
+}
+
+function color2(){
+    color = "#6ff3f8";
+}
+
+function color3(){
+    color = "#1e2140";
+}
