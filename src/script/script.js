431ed8846b28306bf366f6953421eafd0638a0c0
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-02-29 15:13:35 -0300

    feat: added javascript

---
+++ src/script/script.js
@@ -0,0 +1,14 @@
+var pomodoro = 0;
+var short = 0;
+var long = 0;
+
+function Apply(){
+    pomodoro = document.getElementById('pomodoro').value;
+    short = document.getElementById('shortBreak').value;
+    long = document.getElementById('longBreak').value;
+    console.log(pomodoro);
+    console.log(short);
+    console.log(long);
+}
+
+''
