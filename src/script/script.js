de9e688d71bf870038e0534a0f6fd4e56cd44f25
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-02-29 17:49:44 -0300

    feat: creation time

--- src/script/script.js
+++ src/script/script.js
@@ -8,7 +8,7 @@ function Apply(){
     pomodoro = document.getElementById('pomodoro').value;
     short = document.getElementById('shortBreak').value;
     long = document.getElementById('longBreak').value;
-    document.getElementById("minutes").innerText = pomodoro;
+    document.getElementById("minutes").innerText = pomodoro + ":00";
     document.getElementById("relogio").style.backgroundColor = color;
     document.getElementById("p-pomodoro").style.backgroundColor = color;
     document.getElementById("principal").style.fontFamily = font;
@@ -46,4 +46,30 @@ function Sentings(){
 
 function Close(){
     document.getElementById("window").style.display = "none";
+}
+
+function Time(duration, display){
+    var timer = duration, minutes, seconds;
+    setInterval(function(){
+
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes
+        seconds = seconds < 10 ? "0" + seconds : seconds
+
+        display.textContent = minutes + ":" + seconds;
+
+        if(--timer < 0){
+            timer = duration;
+        }
+
+    }, 1000)
+}
+
+function StartTime(){
+    var duration = 60 * pomodoro;
+    var display = document.querySelector("#minutes");
+
+    Time(duration, display);
 }
