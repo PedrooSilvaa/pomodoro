cd34123bc58d40fdee9d6b3bf1d71b04ce9261d3
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-03-01 12:55:32 -0300

    feat: changes mode

--- src/script/script.js
+++ src/script/script.js
@@ -3,6 +3,8 @@ var short = 0;
 var long = 0;
 var font = "";
 var color = "";
+var modo = 0;
+var tmp = 0;
 
 function Apply(){
     pomodoro = document.getElementById('pomodoro').value;
@@ -11,8 +13,9 @@ function Apply(){
     document.getElementById("minutes").innerText = pomodoro + ":00";
     document.getElementById("relogio").style.backgroundColor = color;
     document.getElementById("p-pomodoro").style.backgroundColor = color;
+    document.getElementById("start").style.backgroundColor = color;
     document.getElementById("principal").style.fontFamily = font;
-    
+    modo = 1;
     Close();
 }
 
@@ -59,17 +62,131 @@ function Time(duration, display){
         seconds = seconds < 10 ? "0" + seconds : seconds
 
         display.textContent = minutes + ":" + seconds;
-
-        if(--timer < 0){
+       
+        // TrocaMod();
+        
+        if(--timer < 0 && modo == 1){
+            modo = 2;
+            console.log(duration);
+            StartTime();
             timer = duration;
         }
+        if(--timer < 0  && modo == 2){
+            modo = 3;
+            console.log(duration);
+            StartTime();
+            timer = duration;
+        }
+        if(--timer < 0  && modo == 3){
+            modo = 4;
+            console.log(duration);
+            StartTime();
+            timer = duration;
+            
+        }
+        if(--timer < 0  && modo == 4){
+            modo = 5;
+            console.log(duration);
+            StartTime();
+            timer = duration;
 
-    }, 1000)
+        }
+        if(--timer < 0  && modo == 5){
+            modo = 6;
+            console.log(duration);
+            StartTime();
+            timer = duration;
+
+        }
+        if(--timer < 0  && modo == 6){
+            modo = 7;
+            console.log(duration);
+            StartTime();
+            timer = duration;
+
+        }
+        if(--timer < 0  && modo == 7){
+            modo = 7;
+            console.log(duration);
+            StartTime();
+            timer = 0;
+            
+        }
+
+    }, 500)
 }
 
 function StartTime(){
-    var duration = 60 * pomodoro;
+    if(modo == 1){
+        var duration = (60 * pomodoro);
+        console.log("modo1");
+    }
+    if(modo == 2){
+        duration = (60 * short);
+        console.log("modo2");
+    }
+    if(modo == 3){
+        duration = 60 * pomodoro;
+        console.log("modo3");
+    }
+    if(modo == 4){
+        duration = 60 * long;
+        console.log("modo4");
+    }
+    if(modo == 5){
+        duration = 60 * pomodoro;
+        console.log("modo5");
+    }
+    if(modo == 6){
+        duration = 60 * short;
+    }
+    if(modo == 7){
+        duration = 60 * pomodoro;
+    }
     var display = document.querySelector("#minutes");
-
     Time(duration, display);
+
 }
+
+
+
+// function ModoP(){
+//     // document.getElementById("p-pomodoro").style.backgroundColor = color;
+//     // document.getElementById("p-shortBreak").style.backgroundColor = "transparent";
+//     // document.getElementById("p-longBreak").style.backgroundColor = "transparent";
+// }
+// function ModoShort(){
+//     // document.getElementById("p-pomodoro").style.backgroundColor = "transparent";
+//     // document.getElementById("p-shortBreak").style.backgroundColor = color;
+// }
+// function ModoLong(){
+//     // document.getElementById("p-pomodoro").style.backgroundColor = "transparent";
+//     // document.getElementById("p-longBreak").style.backgroundColor = color;
+// }
+
+// function TrocaMod(){
+
+//     // if(modo == 1){
+//     //     ModoP();
+//     // }
+//     // if(modo == 2){
+//     //     ModoShort();
+//     // }
+//     // if(modo == 3){
+//     //     ModoP();
+//     // }
+
+//     // if(modo == 4){ 
+//     //     ModoLong();
+//     // }
+
+//     // if(modo == 5){
+//     //     ModoP();
+//     // }
+//     // if(modo == 6){
+//     //     ModoShort();
+//     // }
+//     // if(modo == 7){
+//     //     ModoP();
+//     // }
+// }
